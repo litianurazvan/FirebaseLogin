8c36d7670a530a3c834b0463912ced3bb7a81e21
Razvan Litianu <razvan.litianu@endava.com>
2018-03-09 15:26:17 +0200

    Rename initial ViewController to StartViewController

--- FirebaseLogin/FirebaseLogin/StartViewController.swift
+++ FirebaseLogin/FirebaseLogin/StartViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  StartViewController.swift
 //  FirebaseLogin
 //
 //  Created by Razvan Litianu on 09/03/2018.
@@ -8,18 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class StartViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
